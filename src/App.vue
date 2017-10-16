ef9fac0a9bf3fb40344ce74f5488fb0521c5044b
chao <luanxuechaowd@gmail.com>
2017-10-16 20:42:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+
+<style scoped>
+.layout {
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+}
+
+.layout-logo {
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+}
+
+.layout-nav {
+    width: 420px;
+    margin: 0 auto;
+}
+
+.layout-assistant {
+    width: 300px;
+    margin: 0 auto;
+    height: inherit;
+}
+
+.layout-breadcrumb {
+    padding: 10px 15px 0;
+}
+
+.layout-content {
+    min-height: 200px;
+    margin-left: 15px;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 4px;
+}
+
+.layout-content-main {
+    padding: 10px;
+}
+
+.layout-copy {
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+}
+</style>
+<template>
+    <div id="app">
+        <div class="layout">
+            <Menu mode="horizontal" theme="dark" active-name="1">
+                <div class="layout-logo">
+                </div>
+                <div class="layout-nav">
+                    <MenuItem name="1">
+                    <Icon type="ios-navigate"></Icon>
+                    首页
+                    </MenuItem>
+                    <MenuItem name="2">
+                    <Icon type="ios-calendar"></Icon>
+                    时间轴
+                    </MenuItem>
+                    <MenuItem name="4">
+                    <Icon type="social-javascript"></Icon>
+                    案例
+                    </MenuItem>
+                    <MenuItem name="3">
+                    <Icon type="person"></Icon>
+                    关于我
+                    </MenuItem>
+                </div>
+            </Menu>
+            <router-view />
+            <div class="layout-copy">
+                2011-2016 &copy; TalkingData
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,347 @@
+<template>
+  <div class="hello">
+    <Row>
+      <Col span="12" offset="2" style='min-width:470px;margin-top: 15px;'>
+      <ul class="article-list">
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+        <li class="article-list-li">
+          <a href="user/home.html" class="article-list-avatar">
+            <img src="../assets/images/default.png" alt="">
+          </a>
+          <h2 class="article-tip">
+            <a href="jie/detail.html">这是标题</a>
+          </h2>
+          <p class="article-abstract">
+            这是文章内容的简介部分！！！！！！！！阿斯达啊撒 啊是 阿斯达 阿斯达 的飒飒大胆看上了你懒惰了哪里你了那里看到哪里；是你的；案例可能的；按啦啦你拉倒了你懒死了哪里你看了哪里阿兰的李娜；李达康哪里看你兰兰你拉拉手哪里哪里呢
+          </p>
+          <p>
+            <span>
+              <a href="user/home.html">用户昵称</a>
+            </span>
+            <span>1小时前</span>
+            <span>js问题集合</span>
+            <span class="article-list-hint">
+              <a>
+                <Icon type="chatbox-working"></Icon>8
+                <Icon type="ios-eye"></Icon>106
+              </a>
+            </span>
+          </p>
+
+        </li>
+
+      </ul>
+      </Col>
+      <Col span="8" offset="1">
+      <div class="personal-content">
+        <div class="personal-avatar">
+          <img src="../assets/images/timg.jpeg" alt="">
+        </div>
+        <div class="motto">
+          <h2>
+            Try will not die
+          </h2>
+        </div>
+        <div class='contact'>
+           <Tooltip content="luanxuechaowd@gmail.com" placement="right-end">
+            <Icon type="ios-email" size='50'></Icon>
+          </Tooltip>
+          <a href="https://github.com/luanxuechao">
+            <Icon type="social-github" size='50'></Icon>
+          </a>
+          <Tooltip content="微信号:Chevalierwd" placement="top">
+            <Icon type="ios-chatbubble" size='50'></Icon>
+          </Tooltip>
+        </div>
+      </div>
+      <div class="hot-content">
+        <div class="title">
+          <a>热门文章</a>
+        </div>
+        <ul class="hot-content-main">
+          <li>
+            <a>这是一个标题</a>
+          </li>
+          <li>
+            <a>这是一个标题</a>
+          </li>
+          <li>
+            <a>这是一个标题</a>
+          </li>
+          <li>
+            <a>这是一个标题</a>
+          </li>
+          <li>
+            <a>这是一个标题</a>
+          </li>
+        </ul>
+      </div>
+      </Col>
+    </Row>
+  </div>
+</template>
+import  "../assets/css/article.css"
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      movieList: [
+        {
+          name: '肖申克的救赎',
+          url: 'https://movie.douban.com/subject/1292052/',
+          rate: 9.6
+        },
+        {
+          name: '这个杀手不太冷',
+          url: 'https://movie.douban.com/subject/1295644/',
+          rate: 9.4
+        },
+        {
+          name: '霸王别姬',
+          url: 'https://movie.douban.com/subject/1291546/',
+          rate: 9.5
+        },
+        {
+          name: '阿甘正传',
+          url: 'https://movie.douban.com/subject/1292720/',
+          rate: 9.4
+        },
+        {
+          name: '美丽人生',
+          url: 'https://movie.douban.com/subject/1292063/',
+          rate: 9.5
+        },
+        {
+          name: '千与千寻',
+          url: 'https://movie.douban.com/subject/1291561/',
+          rate: 9.2
+        },
+        {
+          name: '辛德勒的名单',
+          url: 'https://movie.douban.com/subject/1295124/',
+          rate: 9.4
+        },
+        {
+          name: '海上钢琴师',
+          url: 'https://movie.douban.com/subject/1292001/',
+          rate: 9.2
+        },
+        {
+          name: '机器人总动员',
+          url: 'https://movie.douban.com/subject/2131459/',
+          rate: 9.3
+        },
+        {
+          name: '盗梦空间',
+          url: 'https://movie.douban.com/subject/3541415/',
+          rate: 9.2
+        }
+      ],
+      randomMovieList: []
+    }
+  },
+  methods: {
+    changeLimit() {
+      function getArrayItems(arr, num) {
+        const temp_array = [];
+        for (let index in arr) {
+          temp_array.push(arr[index]);
+        }
+        const return_array = [];
+        for (let i = 0; i < num; i++) {
+          if (temp_array.length > 0) {
+            const arrIndex = Math.floor(Math.random() * temp_array.length);
+            return_array[i] = temp_array[arrIndex];
+            temp_array.splice(arrIndex, 1);
+          } else {
+            break;
+          }
+        }
+        return return_array;
+      }
+      this.randomMovieList = getArrayItems(this.movieList, 5);
+    }
+  },
+  mounted() {
+    this.changeLimit();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
