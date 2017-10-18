6cc3d9180217c465bd4b0e35d72d15ffddf68f43
chao <luanxuechaowd@gmail.com>
2017-10-18 16:34:36 +0800

    添加回复组件

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+//components
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iview from 'iview'
+import VueQuillEditor from 'vue-quill-editor'
+import mavonEditor from 'mavon-editor'
+
+
+
+// css
 import  'iview/dist/styles/iview.css'
 import './assets/css/article.css'
-import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 import 'github-markdown-css/github-markdown.css'
 import './assets/css/articleDetail.css'
 
+
+// add
 Vue.use(iview);
 Vue.use(mavonEditor)
+Vue.use(VueQuillEditor)
 
 Vue.config.productionTip = false
 
