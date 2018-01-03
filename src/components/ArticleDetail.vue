bf8826a189a86aa280d991a718e99f3259a54824
chao <luanxuechaowd@gmail.com>
2018-01-03 17:09:49 +0800

    add chatRoom

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -1,125 +1,195 @@
 <style>
-	.markdown-body {
-		box-sizing: border-box;
-		min-width: 200px;
-		max-width: 980px;
-		margin: 0 auto;
-		padding: 45px;
-	}
+  .markdown-body {
+    box-sizing: border-box;
+    min-width: 200px;
+    max-width: 980px;
+    margin: 0 auto;
+    padding: 45px;
+  }
 
-	@media (max-width: 767px) {
-		.markdown-body {
-			padding: 15px;
-		}
-	}
+  @media (max-width: 767px) {
+    .markdown-body {
+      padding: 15px;
+    }
+  }
+
 </style>
 <template>
-<div class="hello" style="margin-top:15px;">
-  <Row>
-  <Col span="16" offset="2" style='min-width:470px;background:#fff'>
-  <div class="article-panel detail-box">
+  <div class="hello" style="margin-top:15px;">
+    <Row>
+      <Col span="16" offset="2" style='min-width:470px;background:#fff'>
+      <div class="article-panel detail-box">
         <h1>这个是文章标题</h1>
-        <div class="article-tip article-detail-hint" >
+        <div class="article-tip article-detail-hint">
           <span class="article-tip-stick">置顶帖</span>
           <span class="article-tip-jing">精帖</span>
           <span>js问题集合</span>
           <div class="article-list-hint">
-           <Icon type="chatbox-working" size=18 ></Icon>8
-            <Icon type="ios-eye" size=18  ></Icon>106
-          </div>
+            <Icon type="chatbox-working" size=18></Icon>8
+            <Icon type="ios-eye" size=18></Icon>106 </div>
         </div>
-  </div>
-  <div class="markdown-body">
-      <h2>开发环境状态 develop branch status</h2>
-      <p><a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/develop"><img alt="pipeline status" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/develop/pipeline.svg"></a><br> <a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/develop"><img alt="coverage report" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/develop/coverage.svg"></a></p> <h2>生产环境状态 master branch status</h2> <p><a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/master"><img alt="pipeline status" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/master/pipeline.svg"></a><br> <a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/master"><img alt="coverage report" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/master/coverage.svg"></a></p> <h2>本镜像使用了以下基础镜像:</h2> <ul> <li>phusion/passenger-nodejs:0.9.25</li> <li>monogo:3.4.8</li> <li>redis:4.0.1</li> </ul> <h2>使用前的准备工作</h2> <p>在命令行运行以下命令创建需要的宿主数据卷(Volumes)，并赋予权限：</p> <pre class="hljs"><code class="">$ sudo mkdir /srv &amp;&amp; sudo mkdir /srv/docker $ sudo mkdir /srv/docker/ncwz $ sudo chown yourusername:yourusergroup /srv/docker/ </code></pre> <p>本地 MacOs 开发环境下，请至 Docker Preferences 选中 File Sharing 标签页，<br> 添加绑定的本地数据卷 <code>/srv/docker</code></p> <ol> <li>mongodb 的数据将会被保存在 /srv/docker/ncwz/mongo</li> <li>redis 的数据将会被保存在 /srv/docker/ncwz/redis</li> <li>nginx 的 log 数据将会被保存在 /srv/docker/ncwz/nginx</li> </ol> <h2>运行 docker-compose</h2> <p>请确保 Docker 监护进程在运行中，至本文件夹目录下，同时在命令行运行以下命令启动：</p> <pre class="hljs"><code class="">$ docker-compose up </code></pre> <p>或运行以下命令结束服务：</p> <pre class="hljs"><code class="">$ docker-compose down </code></pre> <h2>安装初始化数据</h2> <p>运行以下命令重置数据库，注意该命令只能在开发和测试环境使用，请相应修改环境变量:</p> <pre class="hljs"><code class="">$ docker-compose exec api bash -c "NODE_ENV=testing yarn run resetDB" </code></pre> <p>运行以下命令安装数据：</p> <pre class="hljs"><code class="">$ docker-compose exec api bash -c "NODE_ENV=testing yarn run fixture" </code></pre> <h2>运行测试脚本</h2> <p>运行以下命令进行测试，注意该命令只能在开发和测试环境使用，请相应修改环境变量:</p> <pre class="hljs"><code class="">$ docker-compose exec api bash -c "NODE_ENV=testing yarn run test" </code></pre> <h2>ESLINT 检查代码质量</h2> <p>可以使用特定的编辑器自动格式化 JS 文件，尽可能优化代码质量，详细 IBM 教程：<br> https://developer.ibm.com/node/2016/07/27/auto-fixing-formatting-your-javascript-with-eslint/</p> <h2>登录研发中心 www.niujianjinfu.com/lab</h2> <p>用户名: <code>alibaba</code><br> 密码: <code>sdfdsfsdfds</code><br> Token: <code>assdsaasadadasd==</code></p> <h2>注意事项</h2> <ul> <li>请结合 Loopback 官方 API 文档：https://apidocs.strongloop.com/loopback</li> <li>确保使用 yarn 管理 npm 包；</li> <li>测试服务器地址：23131231231，可在 <code>/etc/hosts</code> 添加 <code>52.80.82.215 www.niujianjinfu.com</code> 进行域名访问；</li> <li>生产服务器地址：1231313132 -&gt; www.niujianjinfu.com；</li> </ul>
-  </div>
-  </Col>
-   </Row>
-   <div style="width:100%;background:#f5f7f9;height:15px">
-   </div>
-   <Row>
-     <Col span="16" offset="2" style='min-width:470px;background:#fff'>
-     <div class="hello" style="margin-top:15px;">
-    <div class="article-panel detail-box" style="padding-top: 0;">
-        <a name="comment"></a>
-        <ul class="jieda photos" id="jieda">
-          <li data-id="12" class="jieda-daan">
-            <a name="item-121212121212"></a>
-            <div class="detail-about detail-about-reply">
-              <a class="jie-user" href="">
-                <img src="../assets/images/default.png" alt="">
-                <cite>
-                  <i>纸飞机</i>
-                </cite>
-              </a>
-              <div class="detail-hits">
-                <span>3分钟前</span>
-              </div>
-            </div>
-            <div class="detail-body jieda-body">
-              <p>么么哒</p>
-            </div>
-            <div class="jieda-reply">
-              <span class="jieda-zan zanok" type="zan"><Icon type="thumbsup" size=18></Icon></i><em>12</em></span>
-              <!-- <div class="jieda-admin">
-                <span type="edit">编辑</span>
-                <span type="del">删除</span>
-                <span class="jieda-accept" type="accept">采纳</span>
-              </div> -->
-            </div>
-          </li>
-          <li data-id="13">
-            <a name="item-121212121212"></a>
-            <div class="detail-about detail-about-reply">
-              <a class="jie-user" href="">
-                <img src="../assets/images/default.png" alt="">
-                <cite>
-                  <i>香菇</i>
-                </cite>
-              </a>
-              <div class="detail-hits">
-                <span>刚刚</span>
-              </div>
-            </div>
-            <div class="detail-body jieda-body">
-              <p>蓝瘦</p>
-            </div>
-            <div class="jieda-reply">
-             <span class="jieda-zan" type="zan"><Icon type="thumbsup" size=18></Icon></i><em>12</em></span>
-              <span type="reply" ><Icon type="android-hangout" size=18></Icon></i><em>回复</em></span>
-              <div class="jieda-admin">
-                <span type="edit">编辑</span>
-                <span type="del">删除</span>
-              </div>
-            </div>
-          </li>
-          <!-- <li class="fly-none">没有任何回答</li> -->
+      </div>
+      <div class="markdown-body">
+        <h2>开发环境状态 develop branch status</h2>
+        <p>
+          <a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/develop">
+            <img alt="pipeline status" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/develop/pipeline.svg">
+          </a>
+          <br>
+          <a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/develop">
+            <img alt="coverage report" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/develop/coverage.svg">
+          </a>
+        </p>
+        <h2>生产环境状态 master branch status</h2>
+        <p>
+          <a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/master">
+            <img alt="pipeline status" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/master/pipeline.svg">
+          </a>
+          <br>
+          <a href="https://lab.niuchuangwin.com/be/ncwz-node/commits/master">
+            <img alt="coverage report" src="https://lab.niuchuangwin.com/be/ncwz-node/badges/master/coverage.svg">
+          </a>
+        </p>
+        <h2>本镜像使用了以下基础镜像:</h2>
+        <ul>
+          <li>phusion/passenger-nodejs:0.9.25</li>
+          <li>monogo:3.4.8</li>
+          <li>redis:4.0.1</li>
         </ul>
-    </div>
-     <div class='article-panel detail-box'>
-      <quill-editor v-model="content"
-                ref="myQuillEditor"
-                :options="editorOption"
-                @blur="onEditorBlur($event)"
-                @focus="onEditorFocus($event)"
-                @ready="onEditorReady($event)">
-      </quill-editor>
-    <Button type="info" style="margin-top:15px">提交回答</Button>
-    </div>
-
-</div>
-</Col>
-   </Row>
-</div>
+        <h2>使用前的准备工作</h2>
+        <p>在命令行运行以下命令创建需要的宿主数据卷(Volumes)，并赋予权限：</p>
+        <pre class="hljs"><code class="">$ sudo mkdir /srv &amp;
+&amp;
+sudo mkdir /srv/docker $ sudo mkdir /srv/docker/ncwz $ sudo chown yourusername:yourusergroup /srv/docker/ </code></pre>
+        <p>本地 MacOs 开发环境下，请至 Docker Preferences 选中 File Sharing 标签页，
+          <br>添加绑定的本地数据卷
+          <code>/srv/docker</code>
+        </p>
+        <ol>
+          <li>mongodb 的数据将会被保存在 /srv/docker/ncwz/mongo</li>
+          <li>redis 的数据将会被保存在 /srv/docker/ncwz/redis</li>
+          <li>nginx 的 log 数据将会被保存在 /srv/docker/ncwz/nginx</li>
+        </ol>
+        <h2>运行 docker-compose</h2>
+        <p>请确保 Docker 监护进程在运行中，至本文件夹目录下，同时在命令行运行以下命令启动：</p>
+        <pre class="hljs"><code class="">$ docker-compose up </code></pre>
+        <p>或运行以下命令结束服务：</p>
+        <pre class="hljs"><code class="">$ docker-compose down </code></pre>
+        <h2>安装初始化数据</h2>
+        <p>运行以下命令重置数据库，注意该命令只能在开发和测试环境使用，请相应修改环境变量:</p>
+        <pre class="hljs"><code class="">$ docker-compose exec api bash -c "NODE_ENV=testing yarn run resetDB" </code></pre>
+        <p>运行以下命令安装数据：</p>
+        <pre class="hljs"><code class="">$ docker-compose exec api bash -c "NODE_ENV=testing yarn run fixture" </code></pre>
+        <h2>运行测试脚本</h2>
+        <p>运行以下命令进行测试，注意该命令只能在开发和测试环境使用，请相应修改环境变量:</p>
+        <pre class="hljs"><code class="">$ docker-compose exec api bash -c "NODE_ENV=testing yarn run test" </code></pre>
+        <h2>ESLINT 检查代码质量</h2>
+        <p>可以使用特定的编辑器自动格式化 JS 文件，尽可能优化代码质量，详细 IBM 教程：
+          <br>https: //developer.ibm.com/node/2016/07/27/auto-fixing-formatting-your-javascript-with-eslint/</p>
+        <h2>登录研发中心 www.niujianjinfu.com/lab</h2>
+        <p>用户名 :
+          <code>alibaba</code>
+          <br>密码 :
+          <code>sdfdsfsdfds</code>
+          <br>Token :
+          <code>assdsaasadadasd==</code>
+        </p>
+        <h2>注意事项</h2>
+        <ul>
+          <li>请结合 Loopback 官方 API 文档：https: //apidocs.strongloop.com/loopback</li>
+          <li>确保使用 yarn 管理 npm 包；</li>
+          <li>测试服务器地址：23131231231，可在
+            <code>/etc/hosts</code>添加
+            <code>52.80.82.215 www.niujianjinfu.com</code>进行域名访问；</li>
+          <li>生产服务器地址：1231313132 -&gt; www.niujianjinfu.com；
+          </li>
+        </ul>
+      </div>
+      </Col>
+    </Row>
+    <div style="width:100%;background:#f5f7f9;height:15px"></div>
+    <Row>
+      <Col span="16" offset="2" style='min-width:470px;background:#fff'>
+      <div class="hello" style="margin-top:15px;">
+        <div class="article-panel detail-box" style="padding-top: 0;">
+          <a name="comment"></a>
+          <ul class="jieda photos" id="jieda">
+            <li data-id="12" class="jieda-daan">
+              <a name="item-121212121212"></a>
+              <div class="detail-about detail-about-reply">
+                <a class="jie-user" href="">
+                  <img src="../assets/images/default.png" alt="">
+                  <cite>
+                    <i>纸飞机</i>
+                  </cite>
+                </a>
+                <div class="detail-hits">
+                  <span>3分钟前</span>
+                </div>
+              </div>
+              <div class="detail-body jieda-body">
+                <p>么么哒</p>
+              </div>
+              <div class="jieda-reply">
+                <span class="jieda-zan zanok" type="zan">
+                  <Icon type="thumbsup" size=18></Icon>
+                  </i>
+                  <em>12</em>
+                </span>
+                <!-- <div class="jieda-admin"><span type="edit">编辑</span><span type="del">删除</span><span class="jieda-accept" type="accept">采纳</span></div>--></div>
+            </li>
+            <li data-id="13">
+              <a name="item-121212121212"></a>
+              <div class="detail-about detail-about-reply">
+                <a class="jie-user" href="">
+                  <img src="../assets/images/default.png" alt="">
+                  <cite>
+                    <i>香菇</i>
+                  </cite>
+                </a>
+                <div class="detail-hits">
+                  <span>刚刚</span>
+                </div>
+              </div>
+              <div class="detail-body jieda-body">
+                <p>蓝瘦</p>
+              </div>
+              <div class="jieda-reply">
+                <span class="jieda-zan" type="zan">
+                  <Icon type="thumbsup" size=18></Icon>
+                  </i>
+                  <em>12</em>
+                </span>
+                <span type="reply">
+                  <Icon type="android-hangout" size=18></Icon>
+                  </i>
+                  <em>回复</em>
+                </span>
+                <div class="jieda-admin">
+                  <span type="edit">编辑</span>
+                  <span type="del">删除</span>
+                </div>
+              </div>
+            </li>
+            <!-- <li class="fly-none">没有任何回答</li>-->
+          </ul>
+        </div>
+        <div class='article-panel detail-box'>
+          <quill-editor v-model="content" ref="myQuillEditor" :options="editorOption" @blur="onEditorBlur($event)" @focus="onEditorFocus($event)"
+            @ready="onEditorReady($event)"></quill-editor>
+          <Button type="info" style="margin-top:15px">提交回答</Button>
+        </div>
+      </div>
+      </Col>
+    </Row>
+  </div>
 </template>
-
 <script>
-export default {
- name: 'ArticleDetail',
- data() {
-    return {
+  export default {
+    name: 'ArticleDetail',
+    data() {
+      return {}
     }
-}
-}
-</script>
+  }
 
+</script>
--- src/components/chat/chatRoom.vue
+++ src/components/chat/chatRoom.vue
@@ -1,17 +1,145 @@
 <template>
-  <div style="height:100%" >
-  <Row style='height:300px;border-bottom:1px solid #eee;'>
-    <Col span='24' style="height:100%;border-bottom:1px solid #eee;">
-
-    </Col>
-  </Row>
-   <Row>
-
-  </Row>
+  <div style="height:100%">
+    <Row style='height:290px;'>
+      <Col span='24' style="height:100%;">
+      <div style="height:40px;background:#eee;">
+        <span style="line-height: 40px; margin: 20px auto; font-size:18px;margin-left:10px;font-weight:bold">Chevalier</span>
+        <span style="margin-left:370px;">
+          <Icon type="ios-information-outline" size='20'></Icon>
+        </span>
+      </div>
+      <div style="overflow-y: auto;height:250px;">
+        <div class="left-box">
+          <div class="avatar">
+            <Avatar style="background-color: #87d068" icon="person" size="small" />
+          </div>
+          <p style="width:100%;padding:2px 10px;">
+            asdadadasdasdasddassadasddasdsad123121231321231232131231313123213212312312313221231
+          </p>
+        </div>
+        <div class="left-box">
+          <div class="avatar">
+            <Avatar style="background-color: #87d068" icon="person" size="small" />
+          </div>
+          <p style="width:100%;padding:2px 10px;">
+            asdadadasdasdasddassadasddasdsad123121231321231232131231313123213212312312313221231
+          </p>
+        </div>
+        <div class="left-box">
+          <div class="avatar">
+            <Avatar style="background-color: #87d068" icon="person" size="small" />
+          </div>
+          <p style="width:100%;padding:2px 10px;">
+            asdadadasdasdasddassadasddasdsad123121231321231232131231313123213212312312313221231
+          </p>
+        </div>
+          <div class="left-box">
+          <div class="avatar">
+            <Avatar style="background-color: #87d068" icon="person" size="small" />
+          </div>
+          <p style="width:100%;padding:2px 10px;">
+            asdadadasdasdasddassadasddasdsad123121231321231232131231313123213212312312313221231
+          </p>
+        </div>
+        <div class="left-box">
+          <div class="avatar">
+            <Avatar style="background-color: #87d068" icon="person" size="small" />
+          </div>
+          <p style="width:100%;padding:2px 10px;">
+            asdadadasdasdasddassadasddasdsad123121231321231232131231313123213212312312313221231
+          </p>
+        </div>
+          <div class="right-box">
+          <div class="avatar">
+            <Avatar style="background-color: #87d068" icon="person" size="small" />
+          </div>
+          <p style="width:100%;padding:2px 10px;">
+            asdadadasdasdasddassadasddasdsad123121231321231232131231313123213212312312313221231
+          </p>
+        </div>
+      </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col span='24' style="height:100%">
+      <Input v-model="value6" type="textarea" :autosize="{minRows: 5,maxRows: 5}"></Input>
+      </Col>
+    </Row>
   </div>
 </template>
 <script>
-export default {
+  export default {};
 
-}
 </script>
+<style>
+  .left-box {
+    position: relative;
+    top: 10px;
+    left: 60px;
+    max-width: 300px;
+    min-height: 40px;
+    background: #eee;
+    -moz-border-radius: 12px;
+    -webkit-border-radius: 12px;
+    border-radius: 6px;
+    font-size: 16px;
+    font-weight: 500;
+    word-wrap: break-word;
+    margin-bottom: 10px;
+    /*  */
+  }
+
+  .left-box .avatar {
+    position: absolute;
+    top: 10px;
+    left: -50px;
+  }
+
+  .left-box:before {
+    position: absolute;
+    content: "";
+    width: 0;
+    height: 0;
+    right: 100%;
+    top: 12px;
+    border-top: 8px solid transparent;
+    border-right: 16px solid #eee;
+    border-bottom: 8px solid transparent;
+  }
+
+  .right-box {
+    position: relative;
+    top: 10px;
+    left: 60px;
+    max-width: 300px;
+    min-height: 40px;
+    background: #eee;
+    -moz-border-radius: 12px;
+    -webkit-border-radius: 12px;
+    border-radius: 6px;
+    font-size: 16px;
+    font-weight: 500;
+    word-wrap: break-word;
+    margin-bottom: 10px;
+    /*  */
+  }
+
+  .right-box .avatar {
+    position: absolute;
+    top: 10px;
+    left: -50px;
+  }
+
+  .right-box:after {
+    position: absolute;
+    content: "";
+    width: 0;
+    height: 0;
+    right: 100%;
+    top: 12px;
+    border-top: 8px solid transparent;
+    border-right: 16px solid #eee;
+    border-bottom: 8px solid transparent;
+  }
+</style>
+</style>
