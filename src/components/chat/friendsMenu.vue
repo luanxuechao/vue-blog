2a2d7552e01163827b276f76e0c3d85a8b2891d6
chao <luanxuechaowd@gmail.com>
2018-01-19 16:17:05 +0800

    add newFriend message css

--- src/components/chat/friendsMenu.vue
+++ src/components/chat/friendsMenu.vue
@@ -8,14 +8,16 @@
       <!-- 添加朋友-->
       <div>
         <div class='person' style='height:40px;width:100%;border-bottom:1px solid #eee;line-height:40px;'>
-          <div class="demo-avatar" style="margin-left:10px;">
-            <Avatar style="background-color: #EEA150;margin-top:4px;" icon="person-add" shape="square" />
+          <div class="demo-avatar-badge" style="margin-left:10px;">
+            <Badge v-bind:dot="loading">
+              <Avatar style="background-color: #EEA150;" icon="person-add" shape="square" />
+            </Badge>
           </div>
-          <span style="display:block;margin-top: -50px;margin-left: 50px;">新的朋友
+          <span style="display:block;margin-top:-40px;margin-left: 50px;">新的朋友
           </span>
         </div>
       </div>
-       <!-- 朋友列表-->
+      <!-- 朋友列表-->
       <div>
         <div>
           <span style="margin-left:10px;background-color:#eee;display:block;">A</span>
@@ -68,7 +70,7 @@
             </span>
           </div>
         </div>
-         <div>
+        <div>
           <span style="margin-left:10px;background-color:#eee;display:block;">A</span>
           <div class='person' style='height:40px;width:100%;border-bottom:1px solid #eee;line-height:40px;'>
             <div class="demo-avatar" style="margin-left:10px;">
@@ -89,7 +91,71 @@
       </div>
       </Col>
       <Col span='18' style="height:100%; background:#fff;display:flex;justify-content:center;">
-      <div style="width:90%;height:53px;border-bottom:1px solid #eee;text-align:center;">
+      <!-- 默认界面-->
+      <div v-if='friend' style="height:100%;width:100%;display:flex;justify-content:center;align-items:center;">
+        <Icon type="chatboxes" size='100' style="color:#E1E1E1"></Icon>
+      </div>
+      <!-- 新的朋友界面 -->
+      <div>
+        <div style="height:40px;width:100%;border-bottom:1px solid #eee">
+          <span style="line-height: 40px; margin: 20px auto; font-size:18px;margin-left:10px;font-weight:bold">好友验证消息</span>
+          <span style="margin-left:360px;">
+            <Icon type="ios-trash-outline" size='30' style="color:#278DE9"></Icon>
+          </span>
+        </div>
+        <div style="overflow-y: auto;width:100%;height:370px">
+          <div style="text-align: center;margin-top:20px;">
+            <span style="color:#aaa;">01-11 10:20</span>
+            <div style="width:470px; height:100px;margin:0 auto;border:1px solid #eee;border-radius:5px">
+              <div class="demo-avatar" style="float:left;margin:30px 0px 30px 20px;">
+                <Avatar style="background-color: #9f4f68;" icon="spoon" shape="circle" size='large' />
+              </div>
+              <div style="float:left;margin:30px 0px 30px 0px;">
+                <Button type="text" style="color:#278DE9">哇哈哈</Button>
+                <span>已添加你为好友</span>
+              </div>
+              <div style="float:right;margin:30px 20px 30px 0px;">
+                <!-- <Button type="success">同意</Button>
+              <Button type="error">拒绝</Button> -->
+                <span style="color:#C5C5C5">已添加</span>
+              </div>
+            </div>
+          </div>
+          <div style="text-align: center;margin-top:20px;">
+            <span style="color:#aaa">01-11 10:20</span>
+            <div style="width:470px; height:100px;margin:0 auto;border:1px solid #eee;border-radius:5px">
+              <div class="demo-avatar" style="float:left;margin:30px 0px 30px 20px;">
+                <Avatar style="background-color: #9f4f68;" icon="spoon" shape="circle" size='large' />
+              </div>
+              <div style="float:left;margin:30px 0px 30px 0px;">
+                <Button type="text" style="color:#278DE9">哇哈哈</Button>
+                <span>已添加你为好友</span>
+              </div>
+              <div style="float:right;margin:30px 20px 30px 0px;">
+                <Button type="success">同意</Button>
+                <Button type="error">拒绝</Button>
+              </div>
+            </div>
+          </div>
+           <div style="text-align: center;margin-top:20px;">
+            <span style="color:#aaa">01-11 10:20</span>
+            <div style="width:470px; height:100px;margin:0 auto;border:1px solid #eee;border-radius:5px">
+              <div class="demo-avatar" style="float:left;margin:30px 0px 30px 20px;">
+                <Avatar style="background-color: #9f4f68;" icon="spoon" shape="circle" size='large' />
+              </div>
+              <div style="float:left;margin:30px 0px 30px 0px;">
+                <Button type="text" style="color:#278DE9">哇哈哈</Button>
+                <span>已添加你为好友</span>
+              </div>
+              <div style="float:right;margin:30px 20px 30px 0px;">
+                <span style="color:#C5C5C5">等待验证</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 详细信息-->
+      <div v-if='friend' style="width:90%;height:53px;border-bottom:1px solid #eee;text-align:center;">
         <p style="line-height: 53px;font-size:14px">详细信息</p>
         <div style="margin-top:20px;">
           <Avatar shape="square" icon="person" src="https://i.loli.net/2017/08/21/599a521472424.jpg" class='person-avastar' />
@@ -136,6 +202,7 @@
       return {
         modal6: false,
         loading: true,
+        friend: false,
         friendMobile: ''
       }
     },
