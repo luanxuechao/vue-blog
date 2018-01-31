e84ddf7558921eb70a6624d43fc233b7c69ea07c
chao <luanxuechaowd@gmail.com>
2018-01-31 11:57:19 +0800

    修改聊天室样式

--- src/components/chat/chatRoom.vue
+++ src/components/chat/chatRoom.vue
@@ -9,8 +9,67 @@
             <Icon type="ios-information-outline" size='20'></Icon>
           </div>
         </div>
-        <div style="overflow-y: auto;height:250px;width:500px;">
-          <div v-if='chatMessages.length >0' v-for='chatMessage in chatMessages'>
+        <div style="overflow-y: auto;height:250px;width:500px;margin: 0 auto;">
+        <div v-if='chatMessages.length >0' v-for='chatMessage in chatMessages'>
+         <div class="bubbleItem" v-if='chatMessage.sender.id != userId'>
+            <span class="bubble leftBubble">
+              {{chatMessage.messageContent}}
+              <span class="bottomLevel"></span>
+              <span class="topLevel"></span>
+              <span class="chatAvatar">
+                <Avatar style="background-color: #87d068;" icon="person" size="small" />
+              </span>
+            </span>
+          </div>
+           <div class="bubbleItem clearfix" v-if='chatMessage.sender.id == userId'>
+            <span style="font-family: Arial, Helvetica, sans-serif;">
+            </span>
+            <span class="bubble rightBubble">
+              {{chatMessage.messageContent}}
+              <span class="bottomLevel"></span>
+              <span class="topLevel"></span>
+              <span class="chatAvatar">
+                <Avatar style="background-color: #87d068;" icon="person" size="small" />
+              </span>
+            </span>
+          </div>
+        </div>
+       <!--    <div class="bubbleItem">
+
+            <span class="bubble leftBubble">
+              老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！
+              <span class="bottomLevel"></span>
+              <span class="topLevel"></span>
+              <span class="chatAvatar">
+                <Avatar style="background-color: #87d068;" icon="person" size="small" />
+              </span>
+            </span>
+          </div>
+          <div class="bubbleItem clearfix">
+            <span style="font-family: Arial, Helvetica, sans-serif;">
+            </span>
+            <span class="bubble rightBubble">
+              老师，你好！
+              <span class="bottomLevel"></span>
+              <span class="topLevel"></span>
+              <span class="chatAvatar">
+                <Avatar style="background-color: #87d068;" icon="person" size="small" />
+              </span>
+            </span>
+          </div>
+          <div class="bubbleItem clearfix">
+            <span style="font-family: Arial, Helvetica, sans-serif;">
+            </span>
+            <span class="bubble rightBubble">
+              老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！ 老师，你好！
+              <span class="bottomLevel"></span>
+              <span class="topLevel"></span>
+              <span class="chatAvatar">
+                <Avatar style="background-color: #87d068;" icon="person" size="small" />
+              </span>
+            </span>
+          </div>-->
+          <!--  <div v-if='chatMessages.length >0' v-for='chatMessage in chatMessages'>
             <div class="left-box" v-if='chatMessage.sender.id != userId'>
               <div class="avatar">
                 <Avatar style="background-color: #87d068" icon="person" size="small" />
@@ -27,7 +86,7 @@
                  {{chatMessage.messageContent}}
               </p>
             </div>
-          </div>
+          </div>-->
         </div>
         </Col>
       </Row>
@@ -94,74 +153,91 @@
 
 </script>
 <style>
-  .left-box {
-    position: relative;
-    top: 10px;
-    left: 60px;
+  .bubbleItem {
+    width: 100%;
+  }
+
+  .bubble {
     max-width: 300px;
-    min-height: 40px;
-    background: #eee;
-    -moz-border-radius: 12px;
-    -webkit-border-radius: 12px;
-    border-radius: 6px;
-    font-size: 16px;
-    font-weight: 500;
-    word-wrap: break-word;
-    margin-bottom: 10px;
-
-  }
-
-  .left-box .avatar {
-     position: absolute;
-    top: 10px;
-    left:-50px;
-  }
-
-  .left-box:before {
-    position: absolute;
-    content: "";
-    width: 0;
-    height: 0;
-    right: 100%;
-    top: 12px;
-    border-top: 8px solid transparent;
-    border-right: 16px solid #eee;
-    border-bottom: 8px solid transparent;
-  }
-
-  .right-box {
     position: relative;
-    top: 10px;
-    right: -120px;
-    max-width: 300px;
-    min-height: 40px;
-    background: #eee;
-    -moz-border-radius: 12px;
-    -webkit-border-radius: 12px;
-    border-radius: 6px;
-    font-size: 16px;
-    font-weight: 500;
+    line-height: 30px;
+    padding-left: 10px;
+    padding-top: 3px;
+    padding-bottom: 3px;
+    border-radius: 7px;
+    margin-top: 15px;
+    padding-right: 10px;
+    display: inline-block;
+  }
+
+  .leftBubble {
+    position: relative;
+    margin-left: 50px;
+    border: 1px solid #eee;
+    background-color: #eee;
     word-wrap: break-word;
-    margin-bottom: 10px;
-    /*  */
   }
 
-  .right-box .avatar {
+  .leftBubble .bottomLevel {
     position: absolute;
     top: 10px;
-    right: -50px;
+    left: -10px;
+    border-top: 10px solid #eee;
+    border-left: 10px solid transparent;
   }
 
-  .right-box:after {
+  .leftBubble .topLevel {
     position: absolute;
-    content: "";
-    width: 0;
+    top: 11px;
+    left: -8px;
+    border-top: 10px solid #eee;
+    border-left: 10px solid transparent;
+    z-index: 100;
+  }
+
+  .leftBubble .chatAvatar {
+    position: absolute;
+    top: -5px;
+    left: -40px;
+  }
+
+  .rightBubble {
+    position: relative;
+    margin-right: 50px;
+    float: right;
+    border: 1px solid #aaa;
+  }
+
+  .rightBubble .bottomLevel {
+    position: absolute;
+    bottom: 11px;
+    right: -10px;
+    border-bottom: 10px solid #aaa;
+    border-right: 10px solid transparent;
+  }
+
+  .rightBubble .topLevel {
+    position: absolute;
+    bottom: 12px;
+    right: -8px;
+    border-bottom: 10px solid #fff;
+    border-right: 10px solid transparent;
+    z-index: 100;
+  }
+
+  .rightBubble .chatAvatar {
+    position: absolute;
+    bottom: -5px;
+    right: -40px;
+  }
+
+  .clearfix:after {
+    visibility: hidden;
+    display: block;
+    font-size: 0;
+    content: " ";
+    clear: both;
     height: 0;
-    left: 100%;
-    top: 12px;
-    border-top: 8px solid transparent;
-    border-left: 16px solid #eee;
-    border-bottom: 8px solid transparent;
   }
 
 </style>
